c3ccecd4b7e43b3bd46620c3c4e57b11d8205e8e
Scott Cooper <cooperph@oregonstate.edu>
2017-01-23 01:16:55 -0600

    dominion.c refactor

--- projects/cooperph/dominion/dominion.c
+++ projects/cooperph/dominion/dominion.c
@@ -5,8 +5,6 @@
 #include <math.h>
 #include <stdlib.h>
 
-
-
 int compare(const void* a, const void* b) {
   if (*(int*)a > *(int*)b)
     return 1;
@@ -582,6 +580,103 @@ int drawCard(int player, struct gameState *state)
   return 0;
 }
 
+/******* Adventurer *******/
+int playAdventurer( struct gameState *state, int currentPlayer, int *tempHand )
+{
+   int drawnTreasure = 1, z = 0;
+   while( drawn Treasure < 2)
+   {
+	if( state->deckCount[currentPlayer] < 1){
+	shuffle( currentPlayer, state );
+   	}
+
+   	drawCard( currentPlayer, state );
+	int cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];
+
+   	if( cardDrawn == copper || cardDrawn == silver || cardDrawn == gold){
+   	   drawnTreasure++;
+   	}
+
+   	else{
+	   tempHand[z] = cardDrawn;
+	   state->handCount[currentPlayer]--;
+	   z++;
+       	}
+   }   
+   while( z-1 >= 0){
+	state->discard[currentPlayer][state->discardCount[currentPlayer]++] = tempHand[z-1];
+	z = z - 1;
+   }
+   return 0;
+}
+
+/*********** Smithy ***********/
+int playSmithy( struct gameState *state, int currentPlayer, int handPos)
+{
+   //draw 3 cards
+   for( int i = 0; i < 3; i++)
+   {
+	drawCard( currentPlayer, state );
+   }
+
+   return 0;
+}
+
+/******** Village *********/
+int playVillage( int currentPlayer, int handPos, struct gameState *state )
+{
+   // draw a card
+   drawCard( currentPlayer, state );
+
+   // +2 actions
+   state->numActions = state->numActions + 3;
+
+   // discard the card
+   discardCard( handPos, currentPlayer, state, 0 );
+
+   return 0;
+}
+
+/******* Council Room *******/
+int playCouncilRoom( struct gameState *state, int handPos, int currentPlayer )
+{
+   //+4 cards    
+   for( int i = 0; i < 4; i++ ){
+	drawCard( 0, state );
+   }
+
+   //+1 Buy
+   state->numBuys++;
+
+   //Each other player draws a card
+   for( int j = 0; j < state->numPlayers; j++ ){
+	if( j != currentPlayer ){
+	   drawCard( j, state );
+	}
+   }
+
+   // discard card
+   discardCard( handPos, currentPlayer, state, 0);
+
+   return 0;
+}
+
+/****** Great Hall ******/
+int playGreatHall( struct gameState *state, int currentPlayer, int handPos )
+{
+   //+1 card
+   drawCard( currentPlayer, state );
+
+   //+1 Actions
+   state->numActions++;
+
+   // discard card from hand
+   discardCard( handPos-1, currentPlayer, state, 0 );
+
+   return 0;
+}
+
+
 int getCost(int cardNumber)
 {
   switch( cardNumber ) 
@@ -645,139 +740,10 @@ int getCost(int cardNumber)
   return -1;
 }
 
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
   int i;
   int j;
-<<<<<<< Updated upstream
-=======
-//*******************************************************
-// function play_smithy_card
-// Draws three cards, discards, returns
-//******************************************************* 
-int play_smithy_card(struct gameState *state, int currentPlayer, int handPos)
-{
-   int i;
-   for (i = 0; i < 3; i++)
-	   {
-	     drawCard(currentPlayer, state);
-	   }
-   //discard card from hand
-   discardCard(handPos, currentPlayer, state, 0);
-   return 0;
-}
-//******************************************************
-// *function* play_adventurer_card
-// *****************************************************
-int play_adventurer_card(struct gameState *state, int currentPlayer, int drawntreasure, int cardDrawn, int temphand[], int z )
-{
-  while(drawntreasure<2)
-  {
-	  if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-	    shuffle(currentPlayer, state);
-  	}
-  	drawCard(currentPlayer, state);
-  	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-  	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-	    drawntreasure++;
-  	else{
-	    temphand[z]=cardDrawn;
-	    state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-	  // z++ (removed this line to induce a bug, but left this message to be nice )
-	  }
-  }
-  while(z-1>=0){
-	  state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-	  z=z-1;
-  }
-  return 0;
-}			
-//******************************************************
-// **function** play_council_room 
-//******************************************************
-int play_council_room(struct gameState *state, int currentPlayer, int handPos)
-{
-  //+4 Cards
-	int i;
-  for (i = 0; i < 4; i++)
-	{
-	  drawCard(currentPlayer, state);
-	}
-			
-  //+1 Buy Another bug, the line below shouldn't be commented out
-  // state->numBuys++;
-			
-  //Each other player draws a card
-  for (i = 0; i < state->numPlayers; i++)
-	{
-	  if ( i != currentPlayer )
-	    {
-	      drawCard(i, state);
-	    }
-	}
-			
-  //put played card in played card pile
-  discardCard(handPos, currentPlayer, state, 0);
-		
-  return 0;
-}			
-//******************************************************
-// **function** play_remodel
-//******************************************************
-int play_remodel(struct gameState *state, int currentPlayer, int choice1, int choice2, int handPos, int j) 
-{
-  j = state->hand[currentPlayer][choice1];  //store card we will trash
-
-    if ( (getCost(state->hand[currentPlayer][choice1]) + 2) > getCost(choice2) )
-	  {
-	    return 0;
-	  }
-
-    gainCard(choice2, state, 0, currentPlayer);
-
-    //discard card from hand
-    discardCard(handPos, currentPlayer, state, 0);
-
-    //discard trashed card
-    int i;	
-    for (i = 0; i < state->handCount[currentPlayer]; i++)
-  	{
-	    if (state->hand[currentPlayer][i] == j)
-	    {
-	      discardCard(i, currentPlayer, state, 0);			
-	      break;
-	    }
-	  }
-    return 0;
-}			
-//******************************************************
-// **function** play_great_hall
-//******************************************************
-int play_great_hall(int currentPlayer, struct gameState *state, int handPos) 	
-{
-//+1 Card
-  drawCard(currentPlayer, state);
-			
-  //+1 Actions
-  state->numActions++;
-			
-  //discard card from hand
-  discardCard(handPos, currentPlayer, state, 0);
-  return 0;
-} 
-//********************************************************
-
-int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
-{
-  int i;
-  int j=0;
->>>>>>> d708d139123bfc5cf254330da24bb5175cbf1acd
-=======
->>>>>>> Stashed changes
   int k;
   int x;
   int index;
@@ -787,15 +753,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   int tributeRevealedCards[2] = {-1, -1};
   int temphand[MAX_HAND];// moved above the if statement
   int drawntreasure=0;
-<<<<<<< Updated upstream
-<<<<<<< HEAD
   int cardDrawn;
-=======
-  int cardDrawn=0;
->>>>>>> d708d139123bfc5cf254330da24bb5175cbf1acd
-=======
-  int cardDrawn;
->>>>>>> Stashed changes
   int z = 0;// this is the counter for the temp hand
   if (nextPlayer > (state->numPlayers - 1)){
     nextPlayer = 0;
@@ -805,69 +763,12 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   //uses switch to select card and perform actions
   switch( card ) 
     {
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
     case adventurer:
-      while(drawntreasure<2){
-	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-	  shuffle(currentPlayer, state);
-	}
-	drawCard(currentPlayer, state);
-	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-	  drawntreasure++;
-	else{
-	  temphand[z]=cardDrawn;
-	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-	  z++;
-	}
-      }
-      while(z-1>=0){
-	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-	z=z-1;
-      }
-      return 0;
+	playAdventurer( state, currentPlayer, temphand );
 			
     case council_room:
-      //+4 Cards
-      for (i = 0; i < 4; i++)
-	{
-	  drawCard(currentPlayer, state);
-	}
+	playCouncilRoom( state, currentPlayer, handPos );
 			
-      //+1 Buy
-      state->numBuys++;
-			
-      //Each other player draws a card
-      for (i = 0; i < state->numPlayers; i++)
-	{
-	  if ( i != currentPlayer )
-	    {
-	      drawCard(i, state);
-	    }
-	}
-			
-      //put played card in played card pile
-      discardCard(handPos, currentPlayer, state, 0);
-			
-      return 0;
-			
-<<<<<<< Updated upstream
-=======
-//*******************************************************	
-    case adventurer:
-      play_adventurer_card(state, currentPlayer, drawntreasure, cardDrawn, temphand, z);
-      break; 
-//*******************************************************
-    case council_room:
-		  play_council_room(state, currentPlayer, handPos);
-      break;
-//*******************************************************
->>>>>>> d708d139123bfc5cf254330da24bb5175cbf1acd
-=======
->>>>>>> Stashed changes
     case feast:
       //gain card with cost up to 5
       //Backup hand
@@ -958,10 +859,6 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 	}
 			
       return 0;
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
 			
     case remodel:
       j = state->hand[currentPlayer][choice1];  //store card we will trash
@@ -990,39 +887,11 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case smithy:
-      //+3 Cards
-      for (i = 0; i < 3; i++)
-	{
-	  drawCard(currentPlayer, state);
-	}
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
+	playSmithy( state, currentPlayer, handPos );
 		
-<<<<<<< Updated upstream
-=======
-//***********************************************************			
-    case remodel:
-      play_remodel(state, currentPlayer, choice1, choice2, handPos, j); 
-//************************************************************			
-    case smithy:
-		  play_smithy_card(state, currentPlayer, handPos);
-//************************************************************
->>>>>>> d708d139123bfc5cf254330da24bb5175cbf1acd
-=======
->>>>>>> Stashed changes
     case village:
-      //+1 Card
-      drawCard(currentPlayer, state);
-			
-      //+2 Actions
-      state->numActions = state->numActions + 2;
-			
-      //discard played card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
-		
+	playVillage( currentPlayer, handPos, state );	
+	
     case baron:
       state->numBuys++;//Increase buys by 1!
       if (choice1 > 0){//Boolean true or going to discard an estate
@@ -1073,32 +942,10 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 	    
       
       return 0;
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
 		
     case great_hall:
-      //+1 Card
-      drawCard(currentPlayer, state);
-			
-      //+1 Actions
-      state->numActions++;
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
-		
-<<<<<<< Updated upstream
-=======
-//**************************************		
-    case great_hall:
-		  play_great_hall(currentPlayer, state, handPos);
-//*************************************			
-   		
->>>>>>> d708d139123bfc5cf254330da24bb5175cbf1acd
-=======
->>>>>>> Stashed changes
+	playGreatHall( struct gameState *state, int currentPlayer, int handPos);	
+     
     case minion:
       //+1 action
       state->numActions++;
@@ -1515,14 +1362,6 @@ int updateCoins(int player, struct gameState *state, int bonus)
   return 0;
 }
 
-<<<<<<< Updated upstream
-<<<<<<< HEAD
 
-=======
-//******************************************************
->>>>>>> d708d139123bfc5cf254330da24bb5175cbf1acd
-=======
-
->>>>>>> Stashed changes
 //end of dominion.c
 
